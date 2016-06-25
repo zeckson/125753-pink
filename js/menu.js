fb3088fdd31e226eeffcb98054247d6782459604
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-06-25 20:20:47 +0300

    animate menu

--- js/menu.js
+++ js/menu.js
@@ -3,13 +3,23 @@
 
   var menu = document.querySelector(".navigation__menu");
   var hidden = menu.classList.contains("navigation__menu--hidden");
-  document.getElementById("navigation-opener").onclick = function () {
-    hidden = !hidden;
+
+  function toggler() {
     if (hidden) {
       menu.classList.add("navigation__menu--hidden");
     } else {
       menu.classList.remove("navigation__menu--hidden");
     }
+  }
+
+  document.getElementById("navigation-toggle").onclick = function () {
+    hidden = !hidden;
+    toggler();
+    return false;
+  };
+  document.getElementById("navigation-close").onclick = function () {
+    hidden = true;
+    toggler();
     return false;
   }
 })();
